daa0c61b47a84aae8b0717e5b39293b3cb70b7df
videomusik <jakob.bak@gmail.com>
2014-06-10 17:16:04 +0200

    Merge branch 'master' of https://github.com/videomusik/M3T3
    Conflicts:
    	software/lib/M3T3/MotionT3.h

--- software/lib/M3T3/MotionT3.h
+++ software/lib/M3T3/MotionT3.h
@@ -40,7 +40,13 @@ enum SENSOR_INPUT {
     INPUTA4 = 0x0100,
     INPUTA5 = 0x0101,
     INPUTA6 = 0x0110,
-    INPUTA7 = 0x0111    
+    INPUTA7 = 0x0111,
+    INPUTA8 = 0x1000,
+<<<<<<< HEAD
+    INPUTA9 = 0x1001
+=======
+    INPUTA9 = 0x1001    
+>>>>>>> FETCH_HEAD
 };
 
 
